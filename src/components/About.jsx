c18c71d69d49c504f66cd9e4a1b3131682002a13
Poppo7 <146396865+Poppo7@users.noreply.github.com>
2024-09-18 13:39:56 -0700

    About component completed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,10 +5,10 @@ const About = () => {
         <h2 className="text-xl lg:text-3xl tracking-tight text-center uppercase mb-12 mx-4">About</h2>
         <div className="flex flex-col items-center lg:space-x-8 mx-4 mb-20">
             <div className="mb-8 lg:mb-0">
-                <img src={aboutImage} alt="About VastuSpaze" className="w-full h-auto" />
+                <img src={aboutImage} alt="About Binary" className="w-full h-auto" />
                 </div>
                 <p className="text-lg lg:text-3xl font-light text-center lg:text-left max-w-5xl mx-auto mt-8">
-                Laoreet interdum pharetra aliquam curabitur curabitur semper. Mattis adipiscing congue nisi bibendum velit duis efficitur. Vitae ligula eleifend euismod finibus per pellentesque ornare felis. Proin nascetur dignissim lacus vel non potenti ante suspendisse magnis. Faucibus feugiat urna dis; vulputate placerat magnis pulvinar accumsan montes. Vivamus molestie netus a potenti vel. Nibh adipiscing lacinia aliquet orci congue, purus neque finibus. Velit commodo blandit dis mi phasellus pretium.
+                Binary-Brews is a unique coffee shop designed for software engineers, coders, and tech enthusiasts. It's a cozy haven where creativity and productivity flow freely over a cup of expertly brewed coffee. With subtle tech-themed decor, like binary patterns and circuit board designs, it’s the perfect spot to code, collaborate, or unwind. Whether you're debugging your latest project or brainstorming the next big idea, Binary-Brews provides a welcoming environment for all tech lovers.
                 </p>
             </div>
     </section>
